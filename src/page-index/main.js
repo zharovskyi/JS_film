710fa8c203c4ea456d1497010341bcd7a9e3a084
Чудовий Світ <homenkosasha13@gmail.com>
2019-08-15 18:31:51 +0300

    search

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -3,7 +3,8 @@ import '../scss/main.scss';
 import '../scss/header.scss';
 import './page.scss';
 import burgerMenu from './../partials/menu-burger';
-import './api.js'
+import './api.js';
+import './search.js';
 // import apiSearchMovie from '../apiSearchMovie.js'
 // import apiSearchImg from '../apiSearchMovie.js'
 // import apiPopular from '../apiPopular.js'
