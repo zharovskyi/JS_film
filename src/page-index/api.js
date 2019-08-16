c93e466bf9eac99ed8c3e9b570fecb7b387d006d
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-08-16 10:44:56 +0300

    add button with sort film-----oleh

--- src/page-index/api.js
+++ src/page-index/api.js
@@ -1,5 +1,5 @@
-import apiSearchMovie from '../apiSearchMovie.js'
-import apiSearchImg from '../apiSearchImg.js'
+// import apiSearchMovie from '../apiSearchMovie.js'
+// import apiSearchImg from '../apiSearchImg.js'
 import apiPopular from '../apiPopular.js'
 
 import popularHbs from '../partials/popular.hbs'
@@ -12,39 +12,44 @@ const refs = {
   searchForm: document.querySelector('#searchForm'),
   gallery: document.querySelector('.gallery'),
   galleryTv: document.querySelector('.galleryTv'),
-  nav: document.querySelector('.nav__film')
+  nav: document.querySelector('.nav__film'),
 };
 
-console.log(refs.searchForm);
-refs.searchForm.addEventListener('submit', searchSbm)
-refs.nav.addEventListener('click', refresh)
+// refs.searchForm.addEventListener('submit', searchSbm)
+refs.nav.addEventListener('click', refreshFilmChoice);
 
-function searchSbm(e) {
-  e.preventDefault();
-  const inputMovie = e.currentTarget.elements.search.value;
-  console.log('inputMovie :', inputMovie);
-  apiSearchMovie.query = inputMovie;
-  apiSearchMovie.fetch();
-  // const inputId = e.currentTarget.elements.search1.value;
-  // console.log('inputId :', inputId);
-  // apiSearchImg.id = inputId;
-  // apiSearchImg.fetch()
-}
-
-function refresh(e) {
-  e.preventDefault();
-
-  const currentChoise = e.target.dataset.type;
-  console.log('currentChoise :', currentChoise);
-  // markup(currentChoise)
-}
-
+// function searchSbm(e) {
+//   e.preventDefault();
+//   const inputMovie = e.currentTarget.elements.search.value;
+//   clearListItemTV();
+//   console.log('inputMovie :', inputMovie);
+//   apiSearchMovie.query = inputMovie;
+//   apiSearchMovie.fetch();
+//   // const inputId = e.currentTarget.elements.search1.value;
+//   // console.log('inputId :', inputId);
+//   // apiSearchImg.id = inputId;
+//   // apiSearchImg.fetch()
+// }
 function markup() {
+  apiPopular.type = 'movie';
   apiPopular.fetch().then(result => {
-    insertMarkup(result)
+    insertMarkup(result);
   })
 }
-markup()
+markup();
+function refreshFilmChoice(e) {
+  e.preventDefault();
+  const currentChoise = e.target.dataset.type;
+  console.log('currentChoise :', currentChoise);
+  clearListItemFilm();
+  apiPopular.type = currentChoise;
+  apiPopular.fetch().then(result => {
+    
+        insertMarkup(result);
+      })
+  // markup(currentChoise);
+}
+
 
 
 
@@ -52,6 +57,15 @@ function insertMarkup(items) {
   const markup = buildMarkup(items);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
+
+// Build list Iem Hbs
 function buildMarkup(items) {
   return popularHbs(items);
 }
+//  Clear List Item in Ul
+function clearListItemFilm() {
+  refs.gallery.innerHTML = '';
+} 
+function clearListItemTv() {
+  refs.galleryTV.innerHTML = '';
+} 
